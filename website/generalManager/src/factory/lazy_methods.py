294802a451204f5b8c231fb8d9f172ccac602fbf
Tim Kleindick <tkleindick@yahoo.de>
2024-10-19 20:17:58 +0000

    outsourced lazy factory methods

---
+++ website/generalManager/src/factory/lazy_methods.py
@@ -0,0 +1,33 @@
+from factory import LazyFunction, LazyAttribute, LazyAttributeSequence
+import random
+from generalManager.src.measurement.measurement import Measurement
+from datetime import timedelta, date
+from faker import Faker
+
+fake = Faker()
+
+
+def LazyMeasurement(
+    min_value: int | float, max_value: int | float, unit: str
+) -> LazyFunction:
+    return LazyFunction(
+        lambda: Measurement(str(random.uniform(min_value, max_value))[:10], unit)
+    )
+
+
+def LazyDeltaDate(avg_days: int, base_attribute: str) -> LazyAttribute:
+    return LazyAttribute(
+        lambda obj: (getattr(obj, base_attribute) or date.today())
+        + timedelta(days=random.randint(avg_days // 2, avg_days * 3 // 2))
+    )
+
+
+def LazyProjectName() -> LazyFunction:
+    return LazyFunction(
+        lambda: (
+            f"{fake.word().capitalize()} "
+            f"{fake.word().capitalize()} "
+            f"{fake.random_element(elements=('X', 'Z', 'G'))}"
+            f"-{fake.random_int(min=1, max=1000)}"
+        )
+    )
