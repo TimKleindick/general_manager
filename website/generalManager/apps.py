3cc06dcd2c061db091a7cebc88cf2f8f84b50450
Tim Kleindick <tim.kleindick@gmail.com>
2024-11-03 19:27:04 +0000

    refactored apps.py

--- website/generalManager/apps.py
+++ website/generalManager/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 import graphene
-from website.settings import AUTOCREATE_GRAPHQL, GRAPHQL_URL, ROOT_URLCONF
+from django.conf import settings
 from django.urls import path
 from graphene_django.views import GraphQLView
 from importlib import import_module
@@ -11,16 +11,13 @@ class GeneralmanagerConfig(AppConfig):
     name = "generalManager"
 
     def ready(self):
-        if AUTOCREATE_GRAPHQL:
-            # Importieren Sie die Metaklasse
+        if getattr(settings, "AUTOCREATE_GRAPHQL", False):
             from generalManager.src.manager.meta import GeneralManagerMeta
             from generalManager.src.api.graphql import GraphQL
 
-            # Erstellen der GraphQL-Interfaces
             for general_manager_class in GeneralManagerMeta.pending_graphql_interfaces:
                 GraphQL._createGraphQlInterface(general_manager_class)
 
-            # Erstellen der Query-Klasse
             query_class = type("Query", (graphene.ObjectType,), GraphQL._query_fields)
             GraphQL._query_class = query_class
 
@@ -28,11 +25,14 @@ class GeneralmanagerConfig(AppConfig):
             self.add_graphql_url(schema)
 
     def add_graphql_url(self, schema):
-        # Hier erst nach dem Laden der Apps auf die URLs zugreifen
-        urlconf = import_module(ROOT_URLCONF)
+        root_url_conf_path = getattr(settings, "ROOT_URLCONF", None)
+        graph_ql_url = getattr(settings, "GRAPHQL_URL", "graphql/")
+        if not root_url_conf_path:
+            raise Exception("ROOT_URLCONF not found in settings")
+        urlconf = import_module(root_url_conf_path)
         urlconf.urlpatterns.append(
             path(
-                GRAPHQL_URL,
+                graph_ql_url,
                 GraphQLView.as_view(graphiql=True, schema=schema),
             )
         )
