b01d81d512ae5190722e01d09d1e3d12685cb301
Tim Kleindick <tim.kleindick@gmail.com>
2024-11-03 19:03:41 +0000

    removed unnessesary files

--- website/generalManager/urls.py
+++
@@ -1,7 +0,0 @@
-from .schema import schema
-from django.urls import path
-from graphene_django.views import GraphQLView
-
-urlpatterns = [
-    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
-]
--- website/website/urls.py
+++ website/website/urls.py
@@ -21,5 +21,4 @@ from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("generalManager.urls")),
 ]
