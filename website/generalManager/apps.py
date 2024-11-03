17d7997fbd3635e516779590c4e4e1662aec5d2d
Tim Kleindick <tkleindick@yahoo.de>
2024-11-03 19:04:10 +0000

    added new config param GRAPHQL_URL with implicid url generation

--- website/generalManager/apps.py
+++ website/generalManager/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 import graphene
+from website.settings import AUTOCREATE_GRAPHQL, GRAPHQL_URL, ROOT_URLCONF
+from django.urls import path
+from graphene_django.views import GraphQLView
+from importlib import import_module
 
 
 class GeneralmanagerConfig(AppConfig):
@@ -7,19 +11,28 @@ class GeneralmanagerConfig(AppConfig):
     name = "generalManager"
 
     def ready(self):
-        # Importieren Sie die Metaklasse
-        from generalManager.src.manager.meta import GeneralManagerMeta
-        from generalManager.src.api.graphql import GraphQL
+        if AUTOCREATE_GRAPHQL:
+            # Importieren Sie die Metaklasse
+            from generalManager.src.manager.meta import GeneralManagerMeta
+            from generalManager.src.api.graphql import GraphQL
 
-        # Erstellen der GraphQL-Interfaces
-        for general_manager_class in GeneralManagerMeta.pending_graphql_interfaces:
-            GraphQL._createGraphQlInterface(general_manager_class)
+            # Erstellen der GraphQL-Interfaces
+            for general_manager_class in GeneralManagerMeta.pending_graphql_interfaces:
+                GraphQL._createGraphQlInterface(general_manager_class)
 
-        # Erstellen der Query-Klasse
-        query_class = type("Query", (graphene.ObjectType,), GraphQL._query_fields)
-        GraphQL._query_class = query_class
+            # Erstellen der Query-Klasse
+            query_class = type("Query", (graphene.ObjectType,), GraphQL._query_fields)
+            GraphQL._query_class = query_class
 
-        # Schema erstellen
-        from . import schema
+            schema = graphene.Schema(query=GraphQL._query_class)
+            self.add_graphql_url(schema)
 
-        schema.schema = graphene.Schema(query=GraphQL._query_class)
+    def add_graphql_url(self, schema):
+        # Hier erst nach dem Laden der Apps auf die URLs zugreifen
+        urlconf = import_module(ROOT_URLCONF)
+        urlconf.urlpatterns.append(
+            path(
+                GRAPHQL_URL,
+                GraphQLView.as_view(graphiql=True, schema=schema),
+            )
+        )
