ec180a81ad4687fc9f6bbdcbb3557abadcef0229
Tim Kleindick <tkleindick@yahoo.de>
2025-04-21 18:26:05 +0000

    feat: auto redirect to /graphql url

--- website/website/urls.py
+++ website/website/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", lambda request: redirect("graphql/", permanent=True)),
 ]
