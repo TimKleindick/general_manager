8c2c6b901ef2af1fb448a342800fc6dff493a260
Tim Kleindick <tkleindick@yahoo.de>
2025-05-17 14:02:20 +0000

    fix: cache setting logic for nested cached functions

--- src/general_manager/cache/cacheTracker.py
+++ src/general_manager/cache/cacheTracker.py
@@ -4,6 +4,7 @@ from general_manager.cache.dependencyIndex import (
     Dependency,
     filter_type,
 )
+from collections import defaultdict
 
 # Thread-lokale Variable zur Speicherung der Abhängigkeiten
 _dependency_storage = threading.local()
@@ -13,12 +14,23 @@ class DependencyTracker:
     def __enter__(
         self,
     ) -> set[Dependency]:
-        _dependency_storage.dependencies = set()
-        return _dependency_storage.dependencies
+        if not hasattr(_dependency_storage, "dependencies"):
+            _dependency_storage._depth = 0
+            _dependency_storage.dependencies = list()
+        else:
+            _dependency_storage._depth += 1
+        _dependency_storage.dependencies.append(set())
+        return _dependency_storage.dependencies[_dependency_storage._depth]
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if hasattr(_dependency_storage, "dependencies"):
-            del _dependency_storage.dependencies
+            if _dependency_storage._depth == 0:
+                # Wenn wir die oberste Ebene verlassen, löschen wir die Abhängigkeiten
+                del _dependency_storage.dependencies
+            else:
+                # Ansonsten reduzieren wir nur die Tiefe
+                _dependency_storage._depth -= 1
+                _dependency_storage.dependencies.pop()
 
     @staticmethod
     def track(
@@ -30,5 +42,7 @@ class DependencyTracker:
         Adds a dependency to the dependency storage.
         """
         if hasattr(_dependency_storage, "dependencies"):
-            dependencies: set[Dependency] = _dependency_storage.dependencies
-            dependencies.add((class_name, operation, identifier))
+            dependencies: list[set[Dependency]] = _dependency_storage.dependencies
+            depth = _dependency_storage._depth
+            for i in range(depth + 1):
+                dependencies[i].add((class_name, operation, identifier))
