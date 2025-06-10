6e4d441117b7d019e567849e4840195c76a087da
Tim Kleindick <tim.kleindick@gmail.com>
2025-06-10 20:18:42 +0000

    refactor: move calculationBucket to own file

--- src/general_manager/interface/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .databaseInterface import DatabaseInterface, ReadOnlyInterface, DatabaseBucket
-# from .calculationInterface import CalculationInterface, CalculationBucket
-# from .readOnlyInterface import ReadOnlyInterface
-# from .databaseInterface import DatabaseInterface
