3557f71990bb6b384645ac91ccb1ccfc845c6c75
Tim Kleindick <tim.kleindick@gmail.com>
2025-06-22 19:45:14 +0000

    fix: sync and warn ReadOnlyInterfaces &, refactor apps.py

--- src/general_manager/apps.py
+++ src/general_manager/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 import graphene
+import os
 from django.conf import settings
 from django.urls import path
 from graphene_django.views import GraphQLView
@@ -9,11 +10,14 @@ from general_manager.manager.meta import GeneralManagerMeta
 from general_manager.manager.input import Input
 from general_manager.api.property import graphQlProperty
 from general_manager.api.graphql import GraphQL
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Type
 from django.core.checks import register
+import logging
 
 if TYPE_CHECKING:
     from general_manager.interface.readOnlyInterface import ReadOnlyInterface
+
+logger = logging.getLogger(__name__)
 
 
 class GeneralmanagerConfig(AppConfig):
@@ -21,7 +25,59 @@ class GeneralmanagerConfig(AppConfig):
     name = "general_manager"
 
     def ready(self):
+        self.handleReadOnlyInterface()
+        self.initializeGeneralManagerClasses()
+        if getattr(settings, "AUTOCREATE_GRAPHQL", False):
+            self.handleGraphQL()
 
+    def handleReadOnlyInterface(self):
+        self.patchReadOnlyInterfaceSync(GeneralManagerMeta.read_only_classes)
+
+        logger.debug("starting to register ReadOnlyInterface schema warnings...")
+        for general_manager_class in GeneralManagerMeta.read_only_classes:
+            read_only_interface: ReadOnlyInterface = general_manager_class.Interface  # type: ignore
+            register(
+                lambda app_configs, model=read_only_interface._model, manager_class=general_manager_class, **kwargs: read_only_interface.ensureSchemaIsUpToDate(
+                    manager_class, model
+                ),
+                "general_manager",
+            )
+
+    @staticmethod
+    def patchReadOnlyInterfaceSync(general_manager_classes: list[Type[GeneralManager]]):
+        from django.core.management.base import BaseCommand
+
+        original_run_from_argv = BaseCommand.run_from_argv
+
+        def run_from_argv_with_sync(self, argv):
+            # Ensure syncData is only called at real run of runserver
+            run_main = os.environ.get("RUN_MAIN") == "true"
+            command = argv[1] if len(argv) > 1 else None
+            if command != "runserver" or run_main:
+                logger.debug("start syncing ReadOnlyInterface data...")
+                for general_manager_class in general_manager_classes:
+                    read_only_interface: ReadOnlyInterface = general_manager_class.Interface  # type: ignore
+                    read_only_interface.syncData()
+                logger.debug("finished syncing ReadOnlyInterface data.")
+
+            return original_run_from_argv(self, argv)
+
+        BaseCommand.run_from_argv = run_from_argv_with_sync
+
+    def initializeGeneralManagerClasses(self):
+        logger.debug("Initializing GeneralManager classes...")
+
+        logger.debug("starting to create attributes for GeneralManager classes...")
+        for (
+            general_manager_class
+        ) in GeneralManagerMeta.pending_attribute_initialization:
+            attributes = general_manager_class.Interface.getAttributes()
+            setattr(general_manager_class, "_attributes", attributes)
+            GeneralManagerMeta.createAtPropertiesForAttributes(
+                attributes.keys(), general_manager_class
+            )
+
+        logger.debug("starting to connect inputs to other general manager classes...")
         for general_manager_class in GeneralManagerMeta.all_classes:
             attributes = getattr(general_manager_class.Interface, "input_fields", {})
             for attribute_name, attribute in attributes.items():
@@ -40,50 +96,30 @@ class GeneralmanagerConfig(AppConfig):
                         graphQlProperty(func),
                     )
 
-        for general_manager_class in GeneralManagerMeta.read_only_classes:
-            read_only_interface: ReadOnlyInterface = general_manager_class.Interface  # type: ignore
-            register(
-                lambda app_configs, model=read_only_interface._model, manager_class=general_manager_class, **kwargs: read_only_interface.ensureSchemaIsUpToDate(
-                    manager_class, model
-                ),
-                "general_manager",
-            )
+    def handleGraphQL(self):
 
-        for (
-            general_manager_class
-        ) in GeneralManagerMeta.pending_attribute_initialization:
-            attributes = general_manager_class.Interface.getAttributes()
-            setattr(general_manager_class, "_attributes", attributes)
-            GeneralManagerMeta.createAtPropertiesForAttributes(
-                attributes.keys(), general_manager_class
-            )
+        for general_manager_class in GeneralManagerMeta.pending_graphql_interfaces:
+            GraphQL.createGraphqlInterface(general_manager_class)
+            GraphQL.createGraphqlMutation(general_manager_class)
 
-        if getattr(settings, "AUTOCREATE_GRAPHQL", False):
+        query_class = type("Query", (graphene.ObjectType,), GraphQL._query_fields)
+        GraphQL._query_class = query_class
 
-            for general_manager_class in GeneralManagerMeta.pending_graphql_interfaces:
-                GraphQL.createGraphqlInterface(general_manager_class)
-                GraphQL.createGraphqlMutation(general_manager_class)
+        mutation_class = type(
+            "Mutation",
+            (graphene.ObjectType,),
+            {name: mutation.Field() for name, mutation in GraphQL._mutations.items()},
+        )
+        GraphQL._mutation_class = mutation_class
 
-            query_class = type("Query", (graphene.ObjectType,), GraphQL._query_fields)
-            GraphQL._query_class = query_class
+        schema = graphene.Schema(
+            query=GraphQL._query_class,
+            mutation=GraphQL._mutation_class,
+        )
+        self.addGraphqlUrl(schema)
 
-            mutation_class = type(
-                "Mutation",
-                (graphene.ObjectType,),
-                {
-                    name: mutation.Field()
-                    for name, mutation in GraphQL._mutations.items()
-                },
-            )
-            GraphQL._mutation_class = mutation_class
-
-            schema = graphene.Schema(
-                query=GraphQL._query_class,
-                mutation=GraphQL._mutation_class,
-            )
-            self.add_graphql_url(schema)
-
-    def add_graphql_url(self, schema):
+    def addGraphqlUrl(self, schema):
+        logging.debug("Adding GraphQL URL to Django settings...")
         root_url_conf_path = getattr(settings, "ROOT_URLCONF", None)
         graph_ql_url = getattr(settings, "GRAPHQL_URL", "graphql/")
         if not root_url_conf_path:
@@ -95,3 +131,4 @@ class GeneralmanagerConfig(AppConfig):
                 GraphQLView.as_view(graphiql=True, schema=schema),
             )
         )
+        logging.debug(f"GraphQL URL '{graph_ql_url}' added to Django settings.")
