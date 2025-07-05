c1b8ae8d3e55c7d7bf27c2b3b141d9c889417ed3
Tim Kleindick <tim.kleindick@gmail.com>
2025-07-05 21:00:46 +0000

    feat: implement DefaultCreateMutationTest with project creation and validation tests

--- tests/integration/test_default_mutations.py
+++ tests/integration/test_default_mutations.py
@@ -1,3 +1,4 @@
+# type: ignore
 from django.contrib.auth import get_user_model
 from django.db.models import CharField, IntegerField
 from general_manager.manager.generalManager import GeneralManager
@@ -16,7 +17,7 @@ class DefaultCreateMutationTest(GeneralManagerTransactionTestCase):
         class TestProject(GeneralManager):
             class Interface(DatabaseInterface):
                 name = CharField(max_length=100)
-                number = IntegerField(null=True)
+                number = IntegerField(null=True, blank=True)
                 budget = MeasurementField(
                     base_unit="EUR",
                 )
@@ -72,3 +73,43 @@ class DefaultCreateMutationTest(GeneralManagerTransactionTestCase):
         self.assertEqual(project.name, "Test Project")
         self.assertEqual(project.number, 42)
         self.assertEqual(project.budget, "2000 EUR")
+        self.assertEqual(project.changed_by, self.user)
+
+    def test_create_project_without_budget(self):
+        variables = {
+            "name": "Test Project",
+            "number": 42,
+            "budget": None,
+        }
+
+        response = self.query(self.create_mutation, variables=variables)
+        self.assertResponseHasErrors(response)
+
+    def test_create_project_without_name(self):
+        variables = {
+            "name": None,
+            "number": 42,
+            "budget": "2000 EUR",
+        }
+
+        response = self.query(self.create_mutation, variables=variables)
+        self.assertResponseHasErrors(response)
+
+    def test_create_project_without_number(self):
+        variables = {
+            "name": "Test Project",
+            "number": None,
+            "budget": "2000 EUR",
+        }
+
+        response = self.query(self.create_mutation, variables=variables)
+        self.assertResponseNoErrors(response)
+        response = response.json()
+        data = response.get("data", {})
+        self.assertTrue(data["createTestProject"]["success"])
+
+        data = data["createTestProject"]["TestProject"]
+        self.assertEqual(data["name"], "Test Project")
+        self.assertIsNone(data["number"])
+        self.assertEqual(data["budget"]["value"], 2000)
+        self.assertEqual(data["budget"]["unit"], "EUR")
