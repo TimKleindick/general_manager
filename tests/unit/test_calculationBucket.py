e164b33d3bbf36d62429b67140a06b150023da4f
Tim Kleindick <tim.kleindick@gmail.com>
2025-09-01 21:39:27 +0000

    test: fix unit test

--- tests/unit/test_calculationBucket.py
+++ tests/unit/test_calculationBucket.py
@@ -256,6 +256,10 @@ class TestGenerateCombinations(TestCase):
         class DynManager:
             Interface = DynInterface
 
+            def __init__(self, **kwargs):
+                self.__dict__.update(kwargs)
+                self.identification = kwargs
+
         DynInterface._parent_class = DynManager
         return CalculationBucket(DynManager)
 
