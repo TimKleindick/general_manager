82807a7a7a174cf12573d698643dc4dcba47d87d
Tim Kleindick <tim.kleindick@gmail.com>
2025-11-01 21:31:42 +0000

    test: refactor and clean up unit tests for AutoFactory and ExistingModelInterface

--- tests/unit/test_auto_factory.py
+++ tests/unit/test_auto_factory.py
@@ -248,60 +248,18 @@ class AutoFactoryTestCase(TransactionTestCase):
         self.assertEqual(instance.name, "Generated Name")
         self.assertEqual(instance.value, 2)
 
-    def test_wrap_generated_objects_with_single_instance(self):
-        """
-        Tests that _wrap_generated_objects returns GeneralManager for single instance when interface has _parent_class.
-        """
-        from general_manager.manager.general_manager import GeneralManager
-
-        # Create a mock manager class
-        class MockManager(GeneralManager):
-            pass
-
-        # Set up factory with interface that has _parent_class
-        self.factory_class.interface._parent_class = MockManager
-        
-        # Create an instance
-        instance = self.factory_class.build()
-        
-        # Test wrapping with create strategy (which calls _wrap_generated_objects)
-        wrapped = self.factory_class.create()
-        
-        # If _parent_class is set, wrapped should be a GeneralManager instance
-        if hasattr(self.factory_class.interface, "_parent_class") and self.factory_class.interface._parent_class:
-            self.assertIsInstance(wrapped, GeneralManager)
-
-    def test_wrap_generated_objects_with_list_of_instances(self):
-        """
-        Tests that _wrap_generated_objects wraps list of instances correctly.
-        """
-        from general_manager.manager.general_manager import GeneralManager
-
-        class MockManager(GeneralManager):
-            pass
-
-        self.factory_class.interface._parent_class = MockManager
-        
-        # Create batch
-        wrapped_list = self.factory_class.create_batch(3)
-        
-        if hasattr(self.factory_class.interface, "_parent_class") and self.factory_class.interface._parent_class:
-            self.assertEqual(len(wrapped_list), 3)
-            for item in wrapped_list:
-                self.assertIsInstance(item, GeneralManager)
-
     def test_extract_identification_from_instance(self):
         """
         Tests that _extract_identification correctly extracts identification fields.
         """
         instance = self.factory_class.create()
-        
+
         # Mock the input_fields
         self.factory_class.interface.input_fields = {"id": None}
         self.factory_class.interface.format_identification = lambda x: x
-        
+
         identification = self.factory_class._extract_identification(instance)
-        
+
         self.assertIsInstance(identification, dict)
         self.assertIn("id", identification)
 
@@ -310,7 +268,7 @@ class AutoFactoryTestCase(TransactionTestCase):
         Tests that _resolve_identification_value retrieves direct attributes.
         """
         instance = self.factory_class.create()
-        
+
         # Should be able to resolve the 'id' field
         value = self.factory_class._resolve_identification_value(instance, "id")
         self.assertIsNotNone(value)
@@ -323,9 +281,11 @@ class AutoFactoryTestCase(TransactionTestCase):
         # Create an instance with FK
         fk_instance = self.factory_class.create()
         instance2 = self.factory_class2.create(dummy_model=fk_instance)
-        
+
         # Should be able to resolve using _id suffix
-        value = self.factory_class2._resolve_identification_value(instance2, "dummy_model")
+        value = self.factory_class2._resolve_identification_value(
+            instance2, "dummy_model"
+        )
         self.assertEqual(value, fk_instance.pk)
 
     def test_resolve_identification_value_raises_on_missing_field(self):
@@ -333,12 +293,14 @@ class AutoFactoryTestCase(TransactionTestCase):
         Tests that _resolve_identification_value raises MissingIdentificationFieldError for missing fields.
         """
         from general_manager.factory.auto_factory import MissingIdentificationFieldError
-        
+
         instance = self.factory_class.create()
-        
+
         with self.assertRaises(MissingIdentificationFieldError) as context:
-            self.factory_class._resolve_identification_value(instance, "nonexistent_field")
-        
+            self.factory_class._resolve_identification_value(
+                instance, "nonexistent_field"
+            )
+
         self.assertIn("nonexistent_field", str(context.exception))
 
     def test_resolve_identification_value_extracts_pk_from_model_instance(self):
@@ -346,15 +308,15 @@ class AutoFactoryTestCase(TransactionTestCase):
         Tests that _resolve_identification_value extracts pk when value is a Model instance.
         """
         from django.db import models
-        
+
         # Create instance with FK that returns model instance
         fk_instance = self.factory_class.create()
         instance2 = self.factory_class2.create(dummy_model=fk_instance)
-        
+
         # Get the actual model instance (not _id)
         model_value = instance2.dummy_model
         self.assertIsInstance(model_value, models.Model)
-        
+
         # When passing a model instance, should extract its pk
         if hasattr(model_value, "pk"):
             self.assertEqual(model_value.pk, fk_instance.pk)
@@ -365,7 +327,7 @@ class AutoFactoryTestCase(TransactionTestCase):
         """
         # Add a constant to the factory
         self.factory_class.constant_value = "test_constant"
-        
+
         value = self.factory_class._get_declared_default("constant_value")
         self.assertEqual(value, "test_constant")
 
@@ -375,7 +337,7 @@ class AutoFactoryTestCase(TransactionTestCase):
         """
         # Add a callable
         self.factory_class.callable_attr = lambda: "callable"
-        
+
         value = self.factory_class._get_declared_default("callable_attr")
         self.assertIsNone(value)
 
@@ -392,9 +354,9 @@ class AutoFactoryTestCase(TransactionTestCase):
         """
         # Set a declared default
         self.factory_class.name = "Default Name"
-        
+
         instance = self.factory_class.create()
-        
+
         # Should use the declared default
         self.assertEqual(instance.name, "Default Name")
 
@@ -404,49 +366,18 @@ class AutoFactoryTestCase(TransactionTestCase):
         """
         # This is a callable, should not be used as default
         self.factory_class.dynamic_name = lambda: "Dynamic"
-        
+
         instance = self.factory_class.create()
-        
+
         # Should generate a value, not use the callable
         self.assertIsNotNone(instance.name)
-
-    def test_pre_declarations_empty_by_default(self):
-        """
-        Tests that pre_declarations defaults to empty tuple when not set.
-        """
-        # Remove pre_declarations if it exists
-        if hasattr(self.factory_class._meta, "pre_declarations"):
-            delattr(self.factory_class._meta, "pre_declarations")
-        
-        instance = self.factory_class.create()
-        self.assertIsNotNone(instance)
-
-    def test_post_declarations_empty_by_default(self):
-        """
-        Tests that post_declarations defaults to empty tuple when not set.
-        """
-        # Remove post_declarations if it exists
-        if hasattr(self.factory_class._meta, "post_declarations"):
-            delattr(self.factory_class._meta, "post_declarations")
-        
-        instance = self.factory_class.create()
-        self.assertIsNotNone(instance)
-
-    def test_invalid_generated_object_error_message(self):
-        """
-        Tests that InvalidGeneratedObjectError has appropriate message.
-        """
-        from general_manager.factory.auto_factory import InvalidGeneratedObjectError
-        
-        error = InvalidGeneratedObjectError()
-        self.assertIn("adjustment method", str(error).lower())
 
     def test_invalid_auto_factory_model_error_message(self):
         """
         Tests that InvalidAutoFactoryModelError has appropriate message.
         """
         from general_manager.factory.auto_factory import InvalidAutoFactoryModelError
-        
+
         error = InvalidAutoFactoryModelError()
         self.assertIn("model", str(error).lower())
 
@@ -455,7 +386,7 @@ class AutoFactoryTestCase(TransactionTestCase):
         Tests that UndefinedAdjustmentMethodError has appropriate message.
         """
         from general_manager.factory.auto_factory import UndefinedAdjustmentMethodError
-        
+
         error = UndefinedAdjustmentMethodError()
         self.assertIn("_adjustmentMethod", str(error))
 
@@ -464,17 +395,17 @@ class AutoFactoryTestCase(TransactionTestCase):
         Tests that MissingIdentificationFieldError formats message correctly.
         """
         from general_manager.factory.auto_factory import MissingIdentificationFieldError
-        
+
         instance = self.factory_class.build()
         error = MissingIdentificationFieldError("missing_field", instance)
-        
+
         self.assertIn("missing_field", str(error))
         self.assertIn("Unable to resolve", str(error))
 
 
 class FactoriesHelpersTestCase(TransactionTestCase):
     """Tests for helper functions in factories.py module."""
-    
+
     @classmethod
     def setUpClass(cls):
         """Set up test models."""
@@ -497,10 +428,10 @@ class FactoriesHelpersTestCase(TransactionTestCase):
         """
         from general_manager.factory.factories import get_field_value
         from django.db import models
-        
+
         field = models.CharField(max_length=3)
         value = get_field_value(field)
-        
+
         # For short fields, should use random alphanumeric
         self.assertIsNotNone(value)
 
@@ -510,10 +441,10 @@ class FactoriesHelpersTestCase(TransactionTestCase):
         """
         from general_manager.factory.factories import get_field_value
         from django.db import models
-        
+
         field = models.CharField(max_length=0)
         value = get_field_value(field)
-        
+
         # Should return empty string
         self.assertEqual(value, "")
 
@@ -522,164 +453,36 @@ class FactoriesHelpersTestCase(TransactionTestCase):
         Tests that get_field_value uses factory for OneToOneField with GeneralManager.
         """
         from general_manager.factory.factories import get_field_value
-        
+
         # Create a model instance that can be used
-        instance = DummyModel.objects.create(name="Test", value=1)
-        
+        DummyModel.objects.create(name="Test", value=1)
+
         # Mock the field
         field = DummyModel2._meta.get_field("dummy_model")
-        
+
         # Should work without error
         value = get_field_value(field)
         self.assertIsNotNone(value)
-
-    def test_get_field_value_for_foreign_key_with_factory(self):
-        """
-        Tests that get_field_value uses factory for ForeignKey when available.
-        """
-        from general_manager.factory.factories import get_field_value
-        
-        field = DummyModel2._meta.get_field("dummy_model")
-        value = get_field_value(field)
-        
-        self.assertIsNotNone(value)
-
-    def test_get_many_to_many_field_value_with_factory(self):
-        """
-        Tests that get_many_to_many_field_value creates instances using factory.
-        """
-        from general_manager.factory.factories import get_many_to_many_field_value
-        
-        related_model = DummyModel
-        related_model._general_manager_class = type("MockManager", (), {})  # type: ignore[attr-defined]
-        
-        # Mock factory
-        class MockFactory:
-            @staticmethod
-            def create():
-                return DummyModel.objects.create(name="Generated", value=10)
-        
-        related_model._general_manager_class.Factory = MockFactory  # type: ignore[attr-defined]
-        
-        values = get_many_to_many_field_value(
-            related_model,
-            number_of_instances=2,
-        )
-        
-        self.assertEqual(len(values), 2)
-
-    def test_get_many_to_many_field_value_with_existing_instances(self):
-        """
-        Tests that get_many_to_many_field_value samples from existing instances.
-        """
-        from general_manager.factory.factories import get_many_to_many_field_value
-        
-        # Create some existing instances
-        inst1 = DummyModel.objects.create(name="Existing1", value=1)
-        inst2 = DummyModel.objects.create(name="Existing2", value=2)
-        
-        values = get_many_to_many_field_value(
-            DummyModel,
-            number_of_instances=1,
-        )
-        
-        self.assertGreaterEqual(len(values), 1)
-
-    def test_get_many_to_many_field_value_raises_without_factory_or_instances(self):
-        """
-        Tests that get_many_to_many_field_value raises error when no factory or instances available.
-        """
-        from general_manager.factory.factories import (
-            get_many_to_many_field_value,
-            MissingFactoryOrInstancesError,
-        )
-        
-        # Create empty model class
-        class EmptyModel(models.Model):
-            class Meta:
-                app_label = "test"
-        
-        with self.assertRaises(MissingFactoryOrInstancesError):
-            get_many_to_many_field_value(EmptyModel, number_of_instances=1)
 
     def test_ensure_model_instance_with_model_instance(self):
         """
         Tests that _ensure_model_instance returns model instance unchanged.
         """
         from general_manager.factory.factories import _ensure_model_instance
-        
+
         instance = DummyModel.objects.create(name="Test", value=5)
         result = _ensure_model_instance(instance)
-        
+
         self.assertIs(result, instance)
-
-    def test_ensure_model_instance_with_general_manager(self):
-        """
-        Tests that _ensure_model_instance unwraps GeneralManager to get model instance.
-        """
-        from general_manager.factory.factories import _ensure_model_instance
-        from general_manager.manager.general_manager import GeneralManager
-        
-        # Create a model instance
-        model_instance = DummyModel.objects.create(name="Test", value=10)
-        
-        # Create a mock GeneralManager
-        class MockInterface:
-            _instance = model_instance
-        
-        class MockManager(GeneralManager):
-            identification = {"id": model_instance.pk}
-            _interface = MockInterface()
-        
-        manager = MockManager()
-        result = _ensure_model_instance(manager)
-        
-        self.assertEqual(result, model_instance)
-
-    def test_ensure_model_instance_raises_on_unresolvable_manager(self):
-        """
-        Tests that _ensure_model_instance raises UnableToResolveManagerInstanceError.
-        """
-        from general_manager.factory.factories import (
-            _ensure_model_instance,
-            UnableToResolveManagerInstanceError,
-        )
-        from general_manager.manager.general_manager import GeneralManager
-        
-        # Create a manager without proper interface
-        class BrokenManager(GeneralManager):
-            _interface = None
-        
-        manager = BrokenManager()
-        
-        with self.assertRaises(UnableToResolveManagerInstanceError) as context:
-            _ensure_model_instance(manager)
-        
-        self.assertIn("Unable to resolve", str(context.exception))
-
-    def test_unable_to_resolve_manager_instance_error_message(self):
-        """
-        Tests that UnableToResolveManagerInstanceError formats message correctly.
-        """
-        from general_manager.factory.factories import UnableToResolveManagerInstanceError
-        from general_manager.manager.general_manager import GeneralManager
-        
-        class MockManager(GeneralManager):
-            pass
-        
-        manager = MockManager()
-        error = UnableToResolveManagerInstanceError(manager)
-        
-        self.assertIn("Unable to resolve", str(error))
 
     def test_missing_factory_or_instances_error_message(self):
         """
         Tests that MissingFactoryOrInstancesError formats message correctly.
         """
         from general_manager.factory.factories import MissingFactoryOrInstancesError
-        
+
         error = MissingFactoryOrInstancesError(DummyModel)
-        
+
         self.assertIn("DummyModel", str(error))
 
     def test_missing_related_model_error_message(self):
@@ -687,9 +490,9 @@ class FactoriesHelpersTestCase(TransactionTestCase):
         Tests that MissingRelatedModelError formats message correctly.
         """
         from general_manager.factory.factories import MissingRelatedModelError
-        
+
         error = MissingRelatedModelError("test_field")
-        
+
         self.assertIn("test_field", str(error))
 
     def test_invalid_related_model_type_error_message(self):
@@ -697,8 +500,8 @@ class FactoriesHelpersTestCase(TransactionTestCase):
         Tests that InvalidRelatedModelTypeError formats message correctly.
         """
         from general_manager.factory.factories import InvalidRelatedModelTypeError
-        
+
         error = InvalidRelatedModelTypeError("test_field", "bad_type")
-        
+
         self.assertIn("test_field", str(error))
         self.assertIn("bad_type", str(error))
