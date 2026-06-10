3173364fbce35abe03abf20fddf158f2ed0a0d38
Tim Kleindick <tim.kleindick@gmail.com>
2026-06-10 17:17:01 +0200

    feat: add calculation run context

--- src/general_manager/_types/cache.py
+++ src/general_manager/_types/cache.py
@@ -4,8 +4,11 @@ from __future__ import annotations
 
 __all__ = [
     "CacheBackend",
+    "CalculationRunContext",
     "DependencyTracker",
     "cached",
+    "current_calculation_run_context",
+    "ensure_calculation_run_context",
     "invalidate_cache_key",
     "record_dependencies",
     "remove_cache_key_from_index",
@@ -14,6 +17,9 @@ __all__ = [
 from general_manager.cache.cache_decorator import CacheBackend
 from general_manager.cache.cache_tracker import DependencyTracker
 from general_manager.cache.cache_decorator import cached
+from general_manager.cache.run_context import CalculationRunContext
+from general_manager.cache.run_context import current_calculation_run_context
+from general_manager.cache.run_context import ensure_calculation_run_context
 from general_manager.cache.dependency_index import invalidate_cache_key
 from general_manager.cache.dependency_index import record_dependencies
 from general_manager.cache.dependency_index import remove_cache_key_from_index
